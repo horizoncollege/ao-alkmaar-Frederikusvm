480d818e3f4cca5a6e6b14268348e14c735885be
Frederikusvm <117975812+Frederikusvm@users.noreply.github.com>
2023-02-16 15:46:02 +0100

    css bijgescherpt

--- index.php
+++ index.php
@@ -12,26 +12,38 @@
 
     <div id="dashboard">
         <h1>Welkom op het dashboard</h1>
-        <p>Hierop kunt U diverse projecten zien die door mij gemaakt zijn.</p>
+        <p>Hierop kunt U de projecten gemaakt door Frederikus van Miert gedurende zijn opleiding zien.</p>
         <p>Dankuwel voor Uw bezoek</p>
     </div>
 
     <div class="grid">
 
-        <div class="applicatievlak" id="galgje">
-           <a href="./projecten/galgje/index.php" target="_blank"><img src="./images/galgje.png" title="galgje"></a>
+        <div class="applicatievlak">
+            <div class="applicatieafbeelding" id="galgje">
+                <a href="./projecten/galgje/index.php" target="_blank"><img src="./images/galgje.png" title="galgje"></a>
+            </div>
+            <h2>Galgje</h2>
         </div>
 
-        <div class="applicatievlak" id="steenpapierschaar">
-            <a href="./projecten/steenpapierschaar/index.html" target="_blank" title="Steen, papier schaar"><img src="./images/sps.png"></a>
+        <div class="applicatievlak">
+            <div class="applicatieafbeelding" id="steenpapierschaar">
+                <a href="./projecten/steenpapierschaar/index.html" target="_blank" title="Steen, papier schaar"><img src="./images/sps.png"></a>
+            </div>
+            <h2>Steen, Papier Schaar</h2>
         </div>
 
-        <div class="applicatievlak" id="burgerschap">
-            <a href="./projecten/burgerschap/index.php" target="_blank" title="burgerschap"><img src="./images/burgerschap.png"></a>
+        <div class="applicatievlak">
+            <div class="applicatieafbeelding" id="burgerschap">
+                <a href="./projecten/burgerschap/index.php" target="_blank" title="burgerschap"><img src="./images/burgerschap.png"></a>
+            </div>
+            <h2>Burgerschap</h2>
         </div>
 
-        <div class="applicatievlak" id="overmij">
-            <a href="./projecten/overmij/index.html" target="_blank" title="Overmij"><img src="./images/person.png"></a>
+        <div class="applicatievlak">
+            <div class="applicatieafbeelding" id="overmij">
+                <a href="./projecten/overmij/index.html" target="_blank" title="Over mij"><img src="./images/person.png"></a>
+            </div>
+            <h2>Over mij</h2>
         </div>
 
     </div>
