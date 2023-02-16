416d7cf6028927c300d1474fb56afdbfdfc23d75
Frederikusvm <117975812+Frederikusvm@users.noreply.github.com>
2023-02-16 16:26:25 +0100

    Icon en anchor toegevoegd

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>document</title>
+    <title>Dashboard</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="images/logo.png">
 
 </head>
 <body>
@@ -22,28 +23,28 @@
             <div class="applicatieafbeelding" id="galgje">
                 <a href="./projecten/galgje/index.php" target="_blank"><img src="./images/galgje.png" title="galgje"></a>
             </div>
-            <h2>Galgje</h2>
+            <h2><a href="./projecten/galgje/index.php" target="_blank">Galgje</a></h2>
         </div>
 
         <div class="applicatievlak">
             <div class="applicatieafbeelding" id="steenpapierschaar">
                 <a href="./projecten/steenpapierschaar/index.html" target="_blank" title="Steen, papier schaar"><img src="./images/sps.png"></a>
             </div>
-            <h2>Steen, Papier Schaar</h2>
+            <h2><a href="./projecten/steenpapierschaar/index.html" target="_blank">Steen, Papier Schaar</a></h2>
         </div>
 
         <div class="applicatievlak">
             <div class="applicatieafbeelding" id="burgerschap">
                 <a href="./projecten/burgerschap/index.php" target="_blank" title="burgerschap"><img src="./images/burgerschap.png"></a>
             </div>
-            <h2>Burgerschap</h2>
+            <h2><a href="./projecten/burgerschap/index.php" target="_blank">Burgerschap</a></h2>
         </div>
 
         <div class="applicatievlak">
             <div class="applicatieafbeelding" id="overmij">
                 <a href="./projecten/overmij/index.html" target="_blank" title="Over mij"><img src="./images/person.png"></a>
             </div>
-            <h2>Over mij</h2>
+            <h2><a href="./projecten/overmij/index.html" target="_blank">Over mij</a></h2>
         </div>
 
     </div>
