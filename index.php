0b56a8c6b77fc34ce85fa5adac9d215321e54b53
Frederikusvm <117975812+Frederikusvm@users.noreply.github.com>
2023-02-16 16:44:51 +0100

    Comments toegevoegd aan de code

--- index.php
+++ index.php
@@ -11,36 +11,37 @@
 </head>
 <body>
 
+    <!-- Titel en omschrijving tekstbalk. -->
     <div id="dashboard">
         <h1>Welkom op het dashboard</h1>
         <p>Hierop kunt U de projecten gemaakt door Frederikus van Miert gedurende zijn opleiding zien.</p>
         <p>Dankuwel voor Uw bezoek</p>
     </div>
 
-    <div class="grid">
+    <div class="grid"> <!-- De grid-div is de flexboxcontainer voor alle knoppen naar de projecten -->
 
-        <div class="applicatievlak">
+        <div class="applicatievlak"> <!-- De link naar PHP galgje -->
             <div class="applicatieafbeelding" id="galgje">
                 <a href="./projecten/galgje/index.php" target="_blank"><img src="./images/galgje.png" title="galgje"></a>
             </div>
             <h2><a href="./projecten/galgje/index.php" target="_blank">Galgje</a></h2>
         </div>
 
-        <div class="applicatievlak">
+        <div class="applicatievlak"> <!-- De link naar PHP steen, papier schaar -->
             <div class="applicatieafbeelding" id="steenpapierschaar">
                 <a href="./projecten/steenpapierschaar/index.html" target="_blank" title="Steen, papier schaar"><img src="./images/sps.png"></a>
             </div>
             <h2><a href="./projecten/steenpapierschaar/index.html" target="_blank">Steen, Papier Schaar</a></h2>
         </div>
 
-        <div class="applicatievlak">
+        <div class="applicatievlak"> <!-- De link naar het burgerschap politieke partij project  -->
             <div class="applicatieafbeelding" id="burgerschap">
                 <a href="./projecten/burgerschap/index.php" target="_blank" title="burgerschap"><img src="./images/burgerschap.png"></a>
             </div>
             <h2><a href="./projecten/burgerschap/index.php" target="_blank">Burgerschap</a></h2>
         </div>
 
-        <div class="applicatievlak">
+        <div class="applicatievlak"> <!-- Het eerste HTML project dat wij gemaakt hebben. De portfoliosite -->
             <div class="applicatieafbeelding" id="overmij">
                 <a href="./projecten/overmij/index.html" target="_blank" title="Over mij"><img src="./images/person.png"></a>
             </div>
